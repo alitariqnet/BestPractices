798e32cf67495550b3fb0a03880763c53828a388
Ali Tariq <alibabapk@aol.com>
2022-06-06 13:19:33 +0500

    update

---
+++ src/main/MorePractice.java
@@ -0,0 +1,18 @@
+package main;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class MorePractice {
+
+	public static void main(String[] args) {
+		Map<String,String> map = new HashMap<String,String>();
+		HashMap<String,String> hash = new HashMap<String,String>();
+		map.put("Hello","World");
+		hash.put("Hello","World");
+		
+	}
+
+}
