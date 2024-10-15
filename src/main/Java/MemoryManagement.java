8e009a69e2a2f4537eb3c8501e6b7dc64ce4dc89
Ali Tariq <alibabapk@aol.com>
2024-10-15 09:32:15 +0500

    Local and Anonymous classes

--- src/main/Java/MemoryManagement.java
+++ src/main/Java/MemoryManagement.java
@@ -2,18 +2,43 @@ package Java;
 
 import org.openjdk.jol.vm.VM;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class MemoryManagement {
-    private static String name = "Ali";
+    private static String str = "Ali";
 
     public static void main(String[] args) {
-        System.out.println(VM.current().addressOf(name));
-        System.out.println(VM.current().addressOf(getName(name)));
-        System.out.println(VM.current().addressOf(getName("Ali")));
-        System.out.println(VM.current().details());
+        System.out.println(VM.current().addressOf(str));
+        System.out.println(VM.current().addressOf(getMemoryAddress(str)));
+        System.out.println(VM.current().addressOf(getMemoryAddress("Ali")));
+//        System.out.println(VM.current().details());
+        System.out.println("----------------");
 
+        List<String> names = new ArrayList<>();
+        names.add("Umar");
+        names.add("Ali");
+        names.add("Hassan");
+        names.add("Hira");
+        for(String name: names){
+            printMemoryAddress(name);
+        }
+        System.out.println("----------------");
+        List<String> _names = new LinkedList<>();
+        _names.add("Umr");
+        _names.add("Ali");
+        _names.add("Hsn");
+        _names.add("Hra");
+        for(String name: _names){
+            printMemoryAddress(name);
+        }
     }
-    public static String getName(String name){
+    public static String getMemoryAddress(String name){
         System.out.println(VM.current().addressOf(name));
         return name;
+    }
+    public static void printMemoryAddress(String name){
+        System.out.println(VM.current().addressOf(name));
     }
 }
